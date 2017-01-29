7452811430cbf81bed087296feb7dbcfe10b79ae
tbrunetti <tonya.brunetti@colorado.edu>
2017-01-28 20:45:50 -0600

    builds decision tree

--- build_decision_tree.py
+++ build_decision_tree.py
@@ -1,8 +1,8 @@
 import argparse
-from time import gmtime, strftime
 import numpy as np
 from sklearn import tree, metrics
 import pydotplus
+import matplotlib as plt
 from sklearn.externals.six import StringIO
 from sklearn.model_selection import cross_val_predict, cross_val_score, train_test_split
 import pandas
@@ -59,6 +59,12 @@ def build_decision_tree(input, outcome):
 	if len(class_names) > 2:
 		print "Predicted classes are not binary, more than 2 classes" + '\n' + str(class_names)
 		print 'True negative, false positive, false negative, true postive confusion matrix will not be created'
+		#matrix = metrics.confusion_matrix(outcome_test, predicted_test_outcome)
+		#plt.ylabel('Ground Truth validation set')
+		#plt.xlabel('Predicted Outcome validation set')
+		#plt.matshow(matrix)
+		#plt.show()
+
 	else:
 		print "Calculating confusion matrix..."
 		# get confusion matrix to show how samples were classified
@@ -77,38 +83,13 @@ def build_decision_tree(input, outcome):
 
 
 def model_cross_validation(input, outcome):
+	
 	output_results.write('------------------CROSS-VALIDATION OF MODEL--------------------' + '\n')
+	
+	# builds new tree with all data using cross-validation
 	make_tree = tree.DecisionTreeClassifier(min_samples_leaf=args.min_samples, max_depth=args.depth, criterion=args.split)
 	cv_scores = cross_val_score(make_tree, input, outcome, cv=args.cross_val, scoring=args.score_method)
+	
+	# write results of cross-validation both individual and mean scores
 	output_results.write(str(args.cross_val) + '-fold cross-validation scores:' + '\n' + str(cv_scores) + '\n')
 	output_results.write('The mean cross-validation score is:' + '\t' + str(cv_scores.mean()))
-
-if __name__ == '__main__':
-	parser=argparse.ArgumentParser("Cleans data and builds and trains decision tree")
-	parser.add_argument('-dataInput', required=True, dest='matrixFile', help='Full path to comma-delimited "matrix" file')
-	parser.add_argument('-predictColumn', required=True, dest='target_outcome', help='Exact column name in dataset to be used as predicted outcome')
-	parser.add_argument('--run_name', default=strftime("%Y-%m-%d__%H:%M:%S", gmtime()), dest='name', type=str, help='name of run to store results')
-	parser.add_argument('--pct_test', default=0.20, dest='pct_test', type=float, help='[FLOAT] 0.0-1.0, percent of data to use for testing classifier, default=0.20')
-	parser.add_argument('--class', default=None, dest='class_names', type=str, help='comma separated list of predicted class names corresponding to predicted_outcome values in ascending order')
-	parser.add_argument('--minLeaf', default=5, dest='min_samples', type=int, help='[INT] Minimum samples in a node for splitting, default=5')
-	parser.add_argument('--maxDepth', default=3, dest='depth', type=int, help='[INT] Maximum depth to build tree, default=3')
-	parser.add_argument('--split_criteria', default='gini', dest='split', help='gini or entropy, default=gini')
-	parser.add_argument('--seed', default=None, dest='seed', type=int, help='[INT] set seed to ensure data is split the same way every run, assuming no changes in input, default=0')
-	parser.add_argument('--cross_val', default=10, dest='cross_val', type=int, help='[INT] for k-fold cross validation of model use, default=10')
-	parser.add_argument('--scoring', default='accuracy', dest='score_method', help='sting indicating how cross validation should be scored, default=accuracy (see sklearn.model_selection)' )
-	#parser.add_argument('--missing_continuous', dest='impute_cont', action='store_true', help='indicates missing values of continuous type are in data set; This option will handle missing values by imputation')
-	#parser.add_argument('--missing_categorical', dest='impute_cat', action='store_true', help='indicates missing values of categorical type are in data set; this option will handle missing values by assigning negative one to NA cells')
-	#parser.add_argument('--rowNames', dest='rownames', action='store_true', help='row names are included in dataInput in first column')
-	#parser.add_argument('--colNames', dest='colnames', action='store_true', help='column names are included in dataInput in first row')
-	args=parser.parse_args()
-	save_parameters = open(str(args.name) + '_run_parameters.txt', 'w')
-	for key in vars(args):
-		save_parameters.write(str(key) + ':' + '\t' + str(vars(args)[key]) + '\n')	
-
-	# files to keep results and run information
-	output_results = open(str(args.name) + '_metrics.txt', 'w')
-	#formatted_matrix = data_formatting.format_input(data=args.matrixFile, rows=args.rownames, cols=args.colnames)
-	
-	#build_decision_tree(data=formatted_matrix, prediction=args.predicted_outcome)
-	data, ground_truth, tree_classifier, data_training, data_test, outcome_training, outcome_test = build_decision_tree(input=args.matrixFile, outcome=args.target_outcome)
-	model_cross_validation(input=data, outcome=ground_truth)
