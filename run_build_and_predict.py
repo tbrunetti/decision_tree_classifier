3c28c1c71a158d9613208a5d831c7f286feda54d
tbrunetti <tonya.brunetti@colorado.edu>
2017-01-31 23:03:24 -0600

    fixed kwargs issue

--- run_build_and_predict.py
+++ run_build_and_predict.py
@@ -14,17 +14,21 @@ def run(**kwargs):
 	for key in vars(args):
 		save_parameters.write(str(key) + ':' + '\t' + str(vars(args)[key]) + '\n')	
 
-	if kwargs['balance_method'] == None:
+	if kwargs['balance'] == None:
 		#build_decision_tree(data=formatted_matrix, prediction=args.predicted_outcome)
-		data, ground_truth, tree_classifier, data_training, data_test, outcome_training, outcome_test = build_decision_tree.build_decision_tree(input=kwargs['matrixFile'], outcome=kwargs['target_outcome'], **kwargs)
+		data, ground_truth, tree_classifier, data_training, data_test, outcome_training, outcome_test = build_decision_tree.build_decision_tree(input=kwargs['matrixFile'], outcome=kwargs['target_outcome'], method=kwargs['balance'], cols=None, **kwargs)
 		build_decision_tree.model_cross_validation(input=data, outcome=ground_truth, **kwargs)
 	
-	
+	else:
+		imbalance_set.check_imbalance(input=kwargs['matrixFile'], outcome=kwargs['target_outcome'], **kwargs)
+		data_resampled, ground_truth_resampled, column_names = imbalance_set.manage_imbalance(input=kwargs['matrixFile'], outcome=kwargs['target_outcome'], method=kwargs['balance'],**kwargs)
+		data, ground_truth, tree_classifier, data_training, data_test, outcome_training, outcome_test = build_decision_tree.build_decision_tree(input=data_resampled, outcome=ground_truth_resampled, method=kwargs['balance'], cols=column_names, **kwargs)
+		build_decision_tree.model_cross_validation(input=data, outcome=ground_truth, **kwargs)
+
 if __name__ == '__main__':
 	parser=argparse.ArgumentParser("Cleans data and builds and trains decision tree")
 	parser.add_argument('-dataInput', required=True, dest='matrixFile', help='Full path to comma-delimited "matrix" file')
 	parser.add_argument('-predictColumn', required=True, dest='target_outcome', help='Exact column name in dataset to be used as predicted outcome')
-	parser.add_argument('--balance_method', default=None, dest='balance_method', help='method for balancing data set')
 	parser.add_argument('--run_name', default=strftime("%Y-%m-%d__%H:%M:%S", gmtime()), dest='name', type=str, help='name of run to store results')
 	parser.add_argument('--pct_test', default=0.20, dest='pct_test', type=float, help='[FLOAT] 0.0-1.0, percent of data to use for testing classifier, default=0.20')
 	parser.add_argument('--class', default=None, dest='class_names', type=str, help='comma separated list of predicted class names corresponding to predicted_outcome values in ascending order')
@@ -34,6 +38,7 @@ if __name__ == '__main__':
 	parser.add_argument('--seed', default=None, dest='seed', type=int, help='[INT] set seed to ensure data is split the same way every run, assuming no changes in input, default=0')
 	parser.add_argument('--cross_val', default=10, dest='cross_val', type=int, help='[INT] for k-fold cross validation of model use, default=10')
 	parser.add_argument('--scoring', default='accuracy', dest='score_method', help='sting indicating how cross validation should be scored, default=accuracy (see sklearn.model_selection)' )
+	parser.add_argument('--balance_method', default=None, dest='balance', help='method for balancing data set')
 	args = parser.parse_args()
 	kwargs = vars(args)
 	run(**kwargs)
